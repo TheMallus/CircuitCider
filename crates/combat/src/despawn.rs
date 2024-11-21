03fa25f5cccfea5dd9f24315f6e4c39bede1ac7d
TheMallus <alostaunauwave@gmail.com>
2024-11-21 13:16:13 -0600

    despawn change

--- crates/combat/src/despawn.rs
+++ crates/combat/src/despawn.rs
@@ -1,4 +1,5 @@
 use bevy::prelude::*;
+use crate::components::{Health, Bullet};
 pub struct DespawnPlugin;
 
 impl Plugin for DespawnPlugin {
@@ -11,8 +12,15 @@ fn despawn_far_away_entities(mut commands: Commands, query: Query<(Entity, &Glob
     for (entity, transform) in query.iter() {
         let distance = transform.translation().distance(Vec3::ZERO);
 
-        if distance > 50.0 {
+        if distance > 10.0 {
             commands.entity(entity).despawn_recursive();
         }
     }
 }
+// fn despawn_entities(mut commands: Commands, query: Query<(Entity, &Health), Without<Bullet>>) {
+//     for (entity, health) in query.iter() {
+//         if health.hp <= 0.0 {
+//             commands.entity(entity).despawn_recursive();
+//         }
+//     }
+// }
